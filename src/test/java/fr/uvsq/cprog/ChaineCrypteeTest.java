c76d0ae655b33e821133bf808084a060472ce6d7
chaima-29 <chairchaima29@gmail.com>
2025-11-06 12:55:48 +0100

    Q8 : ajout des tests JUnit pour ChaineCryptee

--- src/test/java/fr/uvsq/cprog/ChaineCrypteeTest.java
+++ src/test/java/fr/uvsq/cprog/ChaineCrypteeTest.java
@@ -3,6 +3,9 @@ package fr.uvsq.cprog;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * Classe de test pour ChaineCryptee.
+ */
 public class ChaineCrypteeTest {
 
     @Test
@@ -21,5 +24,35 @@ public class ChaineCrypteeTest {
     public void testEspaceNonModifie() {
         ChaineCryptee c = ChaineCryptee.deEnClair("A B", 1);
         assertEquals("B C", c.crypte());
+    }
+
+    @Test
+    public void testBouclageAlphabet() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("XYZ", 3);
+        assertEquals("ABC", c.crypte());
+    }
+
+    @Test
+    public void testChaineVide() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("", 5);
+        assertEquals("", c.crypte());
+    }
+
+    @Test
+    public void testChaineNull() {
+        ChaineCryptee c = ChaineCryptee.deEnClair(null, 5);
+        assertNull(c.crypte());
+    }
+
+    @Test
+    public void testDecalageNul() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("HELLO", 0);
+        assertEquals("HELLO", c.crypte());
+    }
+
+    @Test
+    public void testDecalageSuperieur26() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("ABC", 29);
+        assertEquals("DEF", c.crypte());
     }
 }
